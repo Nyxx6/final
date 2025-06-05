04b7bc16073b12bd2223a9355799893b36afe2ff
Nyx <92603198+Nyxx6@users.noreply.github.com>
2025-06-05 18:00:17 +0100

    Update mininet_topo.py

--- mininet_topo.py
+++ mininet_topo.py
@@ -28,13 +28,17 @@ def create_topology():
     h2 = net.addHost('h2', ip='10.0.0.2/24')
     h3 = net.addHost('h3', ip='10.0.0.3/24')
     h4 = net.addHost('h4', ip='10.0.0.4/24')
+    h5 = net.addHost('h5', ip='10.0.0.5/24')
+    h6 = net.addHost('h6', ip='10.0.0.6/24')
 
     info('*** Creating links\n')
     # Host links
     net.addLink(h1, s1)
     net.addLink(h2, s1)
-    net.addLink(h3, s2)
+    net.addLink(h3, s1)
     net.addLink(h4, s2)
+    net.addLink(h5, s2)
+    net.addLink(h6, s2)
 
     # Switch link
     net.addLink(s1, s2)
